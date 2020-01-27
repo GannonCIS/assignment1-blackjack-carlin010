bfbabaf70722ab45aff8d81c7f8bbf8b8dcbb340
Nathan Carlin <ncarlin@olymtechnologies.com>
2020-01-27 17:59:19 -0500

    Complete hand class structure

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -10,5 +10,29 @@ package blackjack;
  * @author gubotdev
  */
 public class Deck {
+    private Card[] myCards = new Card[52];
+    private String[] ranks = {};
+    private String[] suits = {};
+    private int nextCard = 0;
     
+    public Deck() {
+        
+    }
+    
+    private void initDeck() {
+        
+    }
+    
+    private void shuffle() {
+        
+    }
+    
+    // printDeck is for testing only --delete me
+    public void printDeck() {
+        
+    }
+    
+    public Card dealCard() {
+        return null; // to do: make sure this returns a card!
+    }
 }
